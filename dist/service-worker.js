89703f60c6be78d71c6b2ae909c456c583e1d63b
mohitgupta004 <guptamohi.009@gmail.com>
2019-11-05 20:15:53 +0530

    First Commit

---
+++ dist/service-worker.js
@@ -0,0 +1,23 @@
+importScripts("/precache-manifest.6eb223ea509e31b8f24ab466bee342de.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+self.addEventListener('message', (e) => {
+    if (!e.data) {
+      return;
+    }
+  
+    switch (e.data) {
+      case 'skipWaiting':
+        self.skipWaiting();
+        break;
+      default:
+        // NOOP
+        break;
+    }
+  });
+  
+  workbox.clientsClaim();
+  
+  // The precaching code provided by Workbox.
+  self.__precacheManifest = [].concat(self.__precacheManifest || []);
+  workbox.precaching.suppressWarnings();
+  workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
