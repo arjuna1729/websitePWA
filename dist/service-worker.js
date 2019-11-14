8e3290b2bf62025260c1a52e8afb40881618e3fd
mohitgupta004 <guptamohi.009@gmail.com>
2019-11-14 21:41:22 +0530

    Upcoming event fixed

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -1,4 +1,4 @@
-importScripts("/precache-manifest.480892b105bcf95991097f621945e1ec.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("/precache-manifest.be52ebf9582a058afe67057c8894ab58.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 self.addEventListener('message', (e) => {
     if (!e.data) {
