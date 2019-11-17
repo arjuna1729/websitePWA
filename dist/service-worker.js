b3901914f3dfe3dbc89aa356fce3e2eec90bb973
mohitgupta004 <guptamohi.009@gmail.com>
2019-11-17 12:13:48 +0530

    Fix firebase.json

---
+++ dist/service-worker.js
@@ -0,0 +1,23 @@
+importScripts("/precache-manifest.d755370532f9f0ec7764086c0668b811.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+self.addEventListener('message', (e) => {
+    if (!e.data) {
+      return;
+    }
+
+    switch (e.data) {
+      case 'skipWaiting':
+        self.skipWaiting();
+        break;
+      default:
+        // NOOP
+        break;
+    }
+  });
+
+  workbox.clientsClaim();
+
+  // The precaching code provided by Workbox.
+  self.__precacheManifest = [].concat(self.__precacheManifest || []);
+  workbox.precaching.suppressWarnings();
+  workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
